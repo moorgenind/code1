88c6e1268d4b2c500a10d8873ce986764992b024
moorgenind <info@moorgenindia.co>
2026-05-28 02:17:09 +0530

    fix: remove stray comma in image_map

--- app/image_map.py
+++ app/image_map.py
@@ -346,7 +346,6 @@ IMAGE_MAP = [
     {"family": "oem", "name": "under staircase profile", "id": "1kjWEx0mOy-ufshfG65NCoVVGhgiT4TZK"},
     {"family": "oem", "name": "wall grazer profile", "id": "1-T6xP80vnXyxE75eb99FD_o43_-ZyNU6"},
     {"family": "oem", "name": "wall washer", "id": "1eBIyi3wnHH3H1f6bLhZwNutTvTJI_l4i"},
-,
     {"family": "decorative", "name": "rechargeable table lamp", "id": "1jbP9LQvRC2Um7WUWWe_PnUfChE7L6TT3"},
     {"family": "decorative", "name": "table lamp", "id": "1TVAT9oyQV7PuRNP-E_4UCaUJFCFgBt_l"},
     {"family": "decorative", "name": "floor lamp", "id": "1Jn6TiDfWouCpXipT_sPzfbhyWpqsO0Z-"},
